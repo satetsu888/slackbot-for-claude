0f90d8a4d754a1f64da0bc85675a5f78a8b467d0
satetsu888 <satetsu888@gmail.com>
2024-03-15 20:23:29 +0900

    many fix

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const { Anthropic } = require('@anthropic-ai/sdk')
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
 
 const anthropic = new Anthropic({
@@ -12,20 +12,59 @@ const anthropic = new Anthropic({
 
 app.event("app_mention", async ({ event, context, client, say }) => {
   console.debug("event", event);
+  console.debug("context", context)
 
-  const threadTs = event.thread_ts ? event.thread_ts : event.ts;
+  let messages = [];
+  if (event.thread_ts) {
+    const thread = await client.conversations.replies({
+      channel: event.channel,
+      ts: event.thread_ts,
+    });
+    console.debug("thread", thread);
+
+    messages = thread.messages.map((message) => {
+      return {
+        role: message.bot_id === context.botId ? "assistant" : "user",
+        content: [{type: "text", text: message.text}],
+      }
+    }).reduce((acc, message) => {
+      if (acc.length === 0) {
+        return [message]
+      } else {
+        const last = acc[acc.length - 1]
+        if (last.role === message.role) {
+          last.content = last.content.concat(message.content)
+          return acc
+        } else {
+          return acc.concat([message])
+        }
+      }
+    }, [])
+  } else {
+    messages.push({ role: "user", content: event.text })
+  }
+  console.debug("messages", JSON.stringify(messages, null, 2))
+
+  const threadTs = event.thread_ts ?? event.ts
 
   const message = await anthropic.messages.create({
     max_tokens: 1024,
-    messages: [{ role: "user", content: event.text }],
-    model: process.env.CLOUD_MODEL,
-  });
-  console.debug("message", message);
+    messages: messages,
+    model: process.env.CLAUDE_MODEL,
+  })
+  // const message = {
+  //   content: [
+  //     {
+  //       text: "dummy",
+  //     },
+  //   ],
+  // }
+  console.debug("message", message)
 
   await say({
     text: message.content[0].text,
     thread_ts: threadTs,
-  });
+  })
 });
 
 (async () => {
